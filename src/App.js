de3fdb635f76f573e11da23d131900ae4556b954
ayush-devshark <ayushagarwal.dev@gmail.com>
2021-10-31 03:30:53 +0530

    manage form state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import YoutubeForm from './components/YoutubeForm';
 
 function App() {
-    return <div className='App'></div>;
+    return (
+        <div className='App'>
+            <YoutubeForm />
+        </div>
+    );
 }
 
 export default App;
